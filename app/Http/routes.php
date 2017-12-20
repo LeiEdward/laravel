16979bdb3a7d32445c6f7200866d48fe0ef77f3d
LEIWEI <skrapy50115@gmail.com>
2017-12-20 17:38:24 +0800

    Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user/{id}', function ($id) {
+  return 'User: '.$id;
+});
+
+// Route::get('user/{name?}', function ($name = 'John') {
+//   return $name;
+// });
+
+// Route::get('user/{name}', function ($name) {
+//   return $name;
+// })
+// ->where('name', '[A-Za-z]+');
+
+// Route::get('user/{id}/{name}', function ($id, $name) {
+//   return $name;
+// })
+// ->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
+// Route::get('user/profile', 'UserController@showProfile')->name('profile');
+
+// Route::get('/profile', function() {
+//   return 'profile';
+// });
