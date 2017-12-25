a308d4856b18b2afa5471ca6d16901af6d04a791
LEIWEI <skrapy50115@gmail.com>
2017-12-25 13:57:15 +0800

    Learning

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,22 +19,42 @@ Route::get('user/{id}', function ($id) {
   return 'User: '.$id;
 });
 
-// Route::get('user/{name?}', function ($name = 'John') {
-//   return $name;
-// });
+// 第一個Route
+Route::post('test', function(){
+  echo 'POST';
+});
 
-// Route::get('user/{name}', function ($name) {
-//   return $name;
-// })
-// ->where('name', '[A-Za-z]+');
+// 第二個Route
+Route::get('test', function(){
+  echo '<form method="POST" action="test">';
+  echo '<input type="hidden" name="_token" value="{{'. csrf_token() .'}}">';
+  echo '<input type="submit">';
+  echo '</form>';
+});
 
-// Route::get('user/{id}/{name}', function ($id, $name) {
-//   return $name;
-// })
-// ->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+// 第三個Route
+Route::put('test', function() {
+  echo 'PUT';
+});
+// 第四個Route
+Route::delete('test', function() {
+  echo 'DELETE';
+});
 
-// Route::get('user/profile', 'UserController@showProfile')->name('profile');
+Route::get('customer', function() {
+  $customer = App\Customer::find(1); //抓一個
 
-// Route::get('/profile', function() {
-//   return 'profile';
-// });
+  echo $customer->name . "<br/>"; //可以這樣指定資料
+  echo '<pre>'; //這個html tag 可讓array整齊好看
+  print_r($customer);
+});
+
+Route::get('customer/{id}', function($id) { //這裡取得參數
+  //根據id抓資料
+  // $customer = App\Customer::find($id);
+
+  //select * FROM `customer` WHERE `id` = $id  limit 1,1;
+  $customer = App\Customer::where('id' ,'=' ,$id)->first();
+
+  echo $customer->name . "<br/>";
+});
